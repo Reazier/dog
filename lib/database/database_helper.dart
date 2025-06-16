c882a4c53c3c41e50998bd033cddad16a7b47cd1
linkOrzzz <nothingtosee2022@outlook.com>
2025-06-16 22:34:03 +0800

    feat(database): 创建班级和学生信息表
    - 新增 classes_info 和 students_info 表
    - 添加外键约束，实现级联删除和更新
    - 移除 users 表的创建语句

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -24,18 +24,24 @@ class DatabaseHelper {
     final docDir = await getApplicationDocumentsDirectory();
 
     String dbStr = join(docDir.path, 'local_database.db');
-
     return await openDatabase(dbStr, version: 1, onCreate: _onCreate);
   }
 
   FutureOr<void> _onCreate(Database db, int version) async {
+    await db.execute('PRAGMA foreign_keys = ON');
     await db.execute('''
-      CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT,
-        password TEXT
-      );
-    ''');
+    CREATE TABLE IF NOT EXISTS classes_info(
+    class_number INTEGET NOT NULL UNIQUE,
+    class_name TEXT NOT NULL UNIQUE,
+    PRIMARY KEY(class_name AUTOINCREMENT)
+    );
+    CREATE TABLE IF NOT EXISTS students_info(
+    student_id INTEGET NOT NULL UNIQUE,
+    class_number INTEGET NOT NULL,
+    student_name TEXT NOT NULL,
+    PRIMARY KEY(student_id AUTOINCREMENT),
+    FOREIGN KEY(class_number) REFERENCES classes_info(class_number) 
+    ON DELETE CASCADE ON UPDATE CASCADE
+    )''');
   }
 }
