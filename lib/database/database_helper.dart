7e15c39328ef87c6b0e55fb4c60b2cbc3119bde5
Reazier <18162736852@189.cn>
2025-06-18 07:38:26 +0800

    feat(database): 新增班级和学生信息插入及查询功能
    - 新增 insertClassInfor 方法用于插入班级信息
    - 新增 insertStudentInfo 方法用于插入学生信息
    - 新增 selectClassesInfor 方法用于查询所有班级信息
    - 新增 selectStudentInfor 方法用于查询所有学生信息

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
@@ -11,6 +12,8 @@ class DatabaseHelper {
   factory DatabaseHelper() => _instance;
 
   static Database? _database;
+  static const _strTableClassesName = "classes_infor";
+  static const _strTableStudentName = "students_infor";
 
   Future<Database> get database async {
     if (_database != null) {
@@ -43,6 +46,29 @@ class DatabaseHelper {
     FOREIGN KEY(class_number) REFERENCES classes_info(class_number) 
     ON DELETE CASCADE ON UPDATE CASCADE
     )''');
-    //test git push fuction.
+  }
+
+  void insertClassInfor(String className) async {
+    final db = await database;
+    await db.insert(_strTableClassesName, {'class_name': className});
+  }
+
+  void insertStudentInfo(String className, String studentName) async {
+    final db = await database;
+    await db.insert(_strTableStudentName, {
+      'class_name': className,
+      'student_name': studentName,
+    });
+  }
+
+  //查询整个classInfor表格
+  Future selectClassesInfor() async {
+    final db = await database;
+    return await db.query(_strTableClassesName);
+  }
+
+  Future selectStudentInfor() async {
+    final db = await database;
+    return await db.query(_strTableStudentName);
   }
 }
